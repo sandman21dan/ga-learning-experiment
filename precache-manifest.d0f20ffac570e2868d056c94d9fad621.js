99964222b34ffca23a0643bd6e2a2ac6a3f4f69f
Daniel Perez <danielperezvl@gmail.com>
2019-01-28 10:13:50 +0000

    Updates

---
+++ precache-manifest.d0f20ffac570e2868d056c94d9fad621.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d08ce530fb29c9f69b",
+    "url": "/ga-learning-experiment/static/js/runtime~main.d6d08ce5.js"
+  },
+  {
+    "revision": "ffcd7cfbc07bc2e7674a",
+    "url": "/ga-learning-experiment/static/js/main.ffcd7cfb.chunk.js"
+  },
+  {
+    "revision": "398ba509899bc12d6acc",
+    "url": "/ga-learning-experiment/static/js/1.398ba509.chunk.js"
+  },
+  {
+    "revision": "ffcd7cfbc07bc2e7674a",
+    "url": "/ga-learning-experiment/static/css/main.2281ed3f.chunk.css"
+  },
+  {
+    "revision": "3782036d6581330079d39525486dc2ee",
+    "url": "/ga-learning-experiment/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ga-learning-experiment/precache-manifest.d0f20ffac570e2868d056c94d9fad621.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ga-learning-experiment/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
